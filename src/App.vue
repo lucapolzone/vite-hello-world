c4eaa39d54f6a49bc369f2dfa0469ed881a80abe
lucapolzone <lucapolzone@gmail.com>
2024-02-07 19:06:40 +0100

    feat: bonus - add AppHero

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script>
   import AppTitle from './components/AppTitle.vue';
+  import AppHero from './components/AppHero.vue';
 
   export default {
-    components: { AppTitle },
+    components: { 
+      AppTitle,
+      AppHero
+     },
   };
 </script>
 
 <template>
   <AppTitle />
+  <AppHero />
 </template>
 
 <style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,10 @@
+<script>
+  export default{};
+</script>
+<template>
+  <div class="container">
+    <img class="jumbotron" src="https://toolset.com/wp-content/uploads/2020/05/toolset-homepage-hero-section-example.png" alt="">
+  </div>
+</template>
+<style></style>
+
